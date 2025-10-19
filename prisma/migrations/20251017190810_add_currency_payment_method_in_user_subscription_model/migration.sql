229812782918a35ee8c87daa3664d6d5bbe8c1db
Jahid0178 <zahidul.haque767@gmail.com>
2025-10-19 13:25:27 +0600

    feat(reminders-payment): add automated reminder cron job, email notifications, and payment model
    - Implemented cron scheduler to automatically send subscription renewal reminders every 5 minutes
    - Integrated Nodemailer with Handlebars for dynamic reminder email templates
    - Added Prisma Payment model with enums for Currency and PaymentProvider
    - Linked payments to users and subscriptions with cascading relations
    - Enhanced reliability with error handling and local time adjustment (+06 offset)
    
    Closes #7

--- prisma/migrations/20251017190810_add_currency_payment_method_in_user_subscription_model/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "UserSubscription" ADD COLUMN     "currency" TEXT NOT NULL DEFAULT 'USD',
-ADD COLUMN     "paymentMethod" TEXT;
---
+++ src/lib/mailer.ts
@@ -0,0 +1,57 @@
+import path from "path";
+import nodemailer from "nodemailer";
+import { compile } from "handlebars";
+import hbs from "nodemailer-express-handlebars";
+import config from "../config/config";
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: config.gmail.user,
+    pass: config.gmail.pass,
+  },
+});
+
+// Attach the template engine
+transporter.use(
+  "compile",
+  hbs({
+    viewEngine: {
+      extname: ".hbs",
+      partialsDir: path.resolve(__dirname, "../templates"),
+      defaultLayout: undefined,
+    },
+    viewPath: path.resolve(__dirname, "../templates"),
+    extName: ".hbs",
+  })
+);
+
+export const sendRemainderEmail = async (
+  to: string,
+  subject: string,
+  data: { name: string; serviceName: string; renewalDate: Date; dashboardLink: string }
+) => {
+  try {
+    const mailOptions = {
+      from: `SubSavver 🧠 <${config.gmail.user}>`,
+      to,
+      subject,
+      template: "reminder",
+      context: { ...data },
+    };
+    const info = await transporter.sendMail(mailOptions);
+
+    if (info.rejected.length > 0) {
+      console.log("Email sending failed to: ", info.rejected);
+      throw new Error(`Email sending failed to: ${info.rejected.join(", ")}`);
+    } else {
+      console.log("✅ Email sent successfully to: ", to);
+      // console.log("Message ID: ", info.messageId);
+    }
+  } catch (error: unknown) {
+    console.log("❌ Failed to send email: ", error);
+  }
+};
